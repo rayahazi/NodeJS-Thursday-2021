8ad69dfe673f971abb52f0f99cd993bf85366112
Raya Klein <44358915+rayahazi@users.noreply.github.com>
2021-10-28 17:35:38 +0300

    Delete 02 lesson directory

--- 02 lesson/03_interval.js
+++
@@ -1,7 +0,0 @@
-// interval(function, milliseconds) - we can use setInterval for clocks, timers
-
-const a = ()=>console.log("Task A");
-const b = ()=>console.log("Task B");
-
-setInterval(a, 1000);
-b();
--- 02 lesson/09_=== vs ==.js
+++
@@ -1,6 +0,0 @@
-// == : will compare the content!
-// === : will compare content and type
-
-console.log(1 == 1); // true
-console.log("1" == 1); // true
-console.log("1" === 1); // false
